e270e31ea2274673df97ae333cf19484577043ea
Rodrigo Neto <134714036+RodrigoNet0@users.noreply.github.com>
2024-02-20 18:20:17 +0000

    form concluido

---
+++ main.js
@@ -0,0 +1,20 @@
+    let btn = document.getElementById("myBtn");
+    let modal = document.getElementById("myModal");    
+    let span = document.getElementsByClassName("close")[0];
+
+    
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+
+    
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
